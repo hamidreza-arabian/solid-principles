ff28f454514cac01277f80e8803aaeb447c0d1cd
hamidreza-arabian <41690470+hamidreza-arabian@users.noreply.github.com>
2023-12-30 15:55:51 +0330

    implement I in solid(interface segregation)

---
+++ InterfaceSegregation.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * clint shouldn't be force to implement unused things
+ * */
+class capitan{
+    /**
+     * @param workers $worker
+     * @return void
+     */
+    public function hire(manageable $worker){
+        $worker->manage();
+    }
+}
+
+interface workeableInterface{
+    public function work();
+}
+
+interface sleepableInterface{
+    public function sleep();
+}
+
+interface manageable{
+    public function manage();
+}
+
+
+class humanworker implements workeableInterface , sleepableInterface , manageable {
+    public function work()
+    {
+        echo "can work". "<br/>";
+    }
+
+    public function sleep()
+    {
+        echo "can sleep". "<br/>";
+    }
+
+    public function manage()
+    {
+        $this->work();
+        $this->sleep();
+    }
+}
+
+class RobotWorker implements workeableInterface , manageable {
+
+    public function work()
+    {
+        echo "can work" . "<br/>";
+    }
+    public function manage()
+    {
+        $this->work();
+    }
+
+
+
+}
+
+$cap = new capitan();
+$cap->hire(new humanworker());
+
+$cap->hire(new RobotWorker());
+
